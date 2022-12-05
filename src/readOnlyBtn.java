4299a654dee5eb78d2f1a282000f1be97d62cc02
Niklas <Niklas89@t-online.de>
2022-12-05 10:22:21 +0100

    added Btn and slider added all libraries

---
+++ src/readOnlyBtn.java
@@ -0,0 +1,16 @@
+import controlP5.Button;
+import controlP5.CallbackListener;
+
+public class readOnlyBtn {
+
+    readOnlyBtn(Button btn, CallbackListener listener){
+
+    }
+
+    public boolean isEnabled(){
+    }
+
+    void setDisabled(boolean state){
+
+    }
+}
---
+++ src/readOnlySlider.java
@@ -0,0 +1,15 @@
+import controlP5.ControlListener;
+import controlP5.Slider;
+
+public class readOnlySlider {
+    readOnlySlider(Slider slider, ControlListener listener){
+
+    }
+
+    public boolean isEnabled(){
+    }
+
+    void setDisabled(boolean state){
+
+    }
+}
